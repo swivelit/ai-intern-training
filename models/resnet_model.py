f9ed52fb228568def6f499124400bb340bf7299f
Ajayvenkat5555 <Ajayvenkat5555@gmail.com>
2025-12-17 07:45:29 +0530

    Remove duplicate ResNet files from Team1 root

--- models/resnet_model.py
+++
@@ -1,7 +0,0 @@
-import torch.nn as nn
-from torchvision import models
-
-def get_resnet(num_classes=10):
-    model = models.resnet18(pretrained=True)
-    model.fc = nn.Linear(model.fc.in_features, num_classes)
-    return model
