2414c0840e96c87aa8f0aaeeefc6b74621ac369c
Anjanadevi2024 <anjanadevi61725@gmail.com>
2025-12-18 22:38:41 +0530

    Text sentiment project added

---
+++ Anjana/text sentiment/train_gru.py
@@ -0,0 +1,27 @@
+
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, GRU, Dense
+
+max_features = 10000
+max_len = 200
+
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+x_train = pad_sequences(x_train, maxlen=max_len)
+x_test = pad_sequences(x_test, maxlen=max_len)
+
+model = Sequential([
+    Embedding(max_features, 128),
+    GRU(128),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=2, batch_size=64, validation_split=0.2)
+
+loss, acc = model.evaluate(x_test, y_test)
+print("GRU Test Accuracy:", acc)
+
+model.save("gru_model.h5")
