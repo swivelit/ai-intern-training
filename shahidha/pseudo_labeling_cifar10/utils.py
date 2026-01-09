965fbf812bcfc5f5dc3c15352af6a0de66f10e44
shahidhasha <shahidhasha1498@gmail.com>
2026-01-09 21:05:31 +0400

    Update utils.py

--- shahidha/pseudo_labeling_cifar10/utils.py
+++ shahidha/pseudo_labeling_cifar10/utils.py
@@ -1,14 +1,16 @@
-
 import torch
 
 def train(model, loader, optimizer, criterion, device):
     model.train()
+    total_loss = 0
     for x, y in loader:
         x, y = x.to(device), y.to(device)
         optimizer.zero_grad()
         loss = criterion(model(x), y)
         loss.backward()
         optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(loader)
 
 def evaluate(model, loader, device):
     model.eval()
